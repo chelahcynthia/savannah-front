6298462fec8ad65885e08cbcc87716f6738d9aeb
chelahcynthia <cchelangat989@gmail.com>
2024-01-23 12:45:35 +0300

    Navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,27 @@ const Navbar = () => {
         {link: "Home", path: "home"},
         {link: "Albums", path: "albums"},
         {link: "Register", path: "register"},
-        {link: "Login", path: "login"}
+        
       ]
   return (
-    <nav>
-        <div>
+    <nav className='bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary'>
+        <div className='text-lg container mx-auto flex justify-between items-center font-medium'>
+          <div className='flex space-x-14 items-center'>
            <h1 className='font-bold text-primary text-2xl flex'>Practise</h1> 
-           <ul>
+           <ul className='md:flex space-x-12'>
+            {/* show links using map method. */}
             {
-              navItems.map(({link, path}) => <a key={link} href={path}>{link}</a>)
+              navItems.map(({link, path}) => <a key={link} href={path} className='block 
+              hover:text-gray-300'>{link}</a>)
             }
            </ul>
+           </div> 
+           {/* login */}
+           <div className='space-x-12 hidden md:flex items-center'>
+            <a href='/'><span>Language</span></a>
+            <button className='bg-secondary py-2 px-4 transition-all duration-300 rounded hover:text-white hover:bg-indigo-600'>login</button>
+
+           </div>
         </div>
         
         
