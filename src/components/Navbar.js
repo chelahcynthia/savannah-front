35c120c8c21d1bf3e56269b47c6c2f01e5941ae0
chelahcynthia <cchelangat989@gmail.com>
2024-01-23 21:40:46 +0300

    Installs react icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
     const navItems = [
         {link: "Home", path: "home"},
         {link: "Albums", path: "albums"},
@@ -11,7 +15,7 @@ const Navbar = () => {
     <nav className='bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary'>
         <div className='text-lg container mx-auto flex justify-between items-center font-medium'>
           <div className='flex space-x-14 items-center'>
-           <h1 className='font-bold text-primary text-2xl flex'>Practise</h1> 
+           <h1 className='font-bold text-primary text-2xl flex'>PRACT</h1> 
            <ul className='md:flex space-x-12'>
             {/* show links using map method. */}
             {
@@ -26,6 +30,18 @@ const Navbar = () => {
             <button className='bg-secondary py-2 px-4 transition-all duration-300 rounded hover:text-white hover:bg-indigo-600'>login</button>
 
            </div>
+           {/* menu btn, only display on mobile */}
+           <div className='hidden'>
+            <button onClick={toggleMenu} className='text-white focus:outline-none
+            focus:text-gray-300'>
+              {
+                isMenuOpen ? () => ();
+              }
+
+            </button>
+           </div>
+
+
         </div>
         
         
